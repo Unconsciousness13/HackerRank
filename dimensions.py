4ac9f5624f1ce9339865a0b6e1908d5bd92c8986
unconsciousness13 <pakounicredit@gmail.com>
2023-03-15 14:29:53 +0100

    commit

---
+++ dimensions.py
@@ -0,0 +1,16 @@
+def sockMerchant(n, ar):
+    ar.sort(reverse=False)
+    pairs = 0
+    while True:
+
+
+        return pairs
+
+
+n = int(input().strip())
+
+ar = list(map(int, input().rstrip().split()))
+
+result = sockMerchant(n, ar)
+
+print(str(result) + '\n')
