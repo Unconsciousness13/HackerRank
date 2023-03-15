43c6a998f5ceccc6f63b6ba547d5229dd96feaae
unconsciousness13 <pakounicredit@gmail.com>
2023-03-15 15:42:25 +0100

    solution Infinity string

---
+++ infinity_string_letters.py
@@ -0,0 +1,17 @@
+def repeatedString(s, n):
+    r = 0
+    l = len(s)
+    for i in range(0, l):
+        if s[i] == 'a':
+            r += 1
+    r *= int(n / l)
+    for i in range(0, n % l):
+        if s[i] == 'a':
+            r += 1
+    return r
+
+
+s = input()
+n = int(input().strip())
+result = repeatedString(s, n)
+print(str(result) + '\n')
