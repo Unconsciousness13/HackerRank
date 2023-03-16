88eede790ffb2ae6eebfa2bdcc2c04148be6b42d
unconsciousness13 <pakounicredit@gmail.com>
2023-03-16 12:39:31 +0100

    two more

---
+++ a_very_big_sum.py
@@ -0,0 +1,12 @@
+def aVeryBigSum(ar):
+    # Write your code here
+    pass
+
+
+ar_count = int(input().strip())
+
+ar = list(map(int, input().rstrip().split()))
+
+result = aVeryBigSum(ar)
+
+print(str(result) + '\n')
