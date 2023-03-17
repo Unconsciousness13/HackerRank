d433b6bc3ec4bf1fa660f1f52279ffd710250d38
unconsciousness13 <pakounicredit@gmail.com>
2023-03-17 13:03:51 +0100

    plus minus

---
+++ ProblemSolving/plus_minus.py
@@ -0,0 +1,25 @@
+def plusMinus(arr):
+    negative = 0
+    zeros = 0
+    positive = 0
+
+    for num in arr:
+        if num < 0:
+            negative += 1
+        elif num > 0:
+            positive += 1
+        else:
+            zeros += 1
+    positive_percents = float(100 * positive / n / 100)
+    negative_percents = float(100 * negative / n / 100)
+    zeros_percents = float(100 * zeros / n / 100)
+    print(*positive_percents)
+    print(*negative_percents)
+    print(*zeros_percents)
+
+
+n = int(input().strip())
+
+arr = list(map(int, input().rstrip().split()))
+
+print(plusMinus(arr))
---
+++ ProblemSolving/testss.py
@@ -0,0 +1,3 @@
+num = float(6)
+rounded = round(num, 1)
+print(rounded)
