62e957f07736a819f99953974ca765672f113d12
unconsciousness13 <pakounicredit@gmail.com>
2023-03-17 11:38:31 +0100

    compare the triplets

---
+++ ProblemSolving/compare the triplets.py
@@ -0,0 +1,19 @@
+def compareTriplets(a, b):
+    a_points = 0
+    b_points = 0
+    for i in range(len(a)):
+        if a[i] < b[i]:
+            b_points += 1
+        elif a[i] > b[i]:
+            a_points += 1
+    return (a_points, b_points)
+
+
+a = list(map(int, input().rstrip().split()))
+
+b = list(map(int, input().rstrip().split()))
+
+result = compareTriplets(a, b)
+
+print(' '.join(map(str, result)))
+print('\n')
