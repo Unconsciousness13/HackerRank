162456dc8e14e5ddb866282aaf8c21556c1bb346
unconsciousness13 <pakounicredit@gmail.com>
2023-03-21 12:52:10 +0100

    2d array

--- Arrays/2D-Array-DS.py
+++ Arrays/2D-Array-DS.py
@@ -1,4 +1,14 @@
 def hourglassSum(arr):
+    best = -90
+    for row in range(4):
+        for col in range(4):
+            sum1 = arr[row][col] + arr[row][col+1] + arr[row][col+2]
+            sum2 = arr[row+1][col+1]
+            sum3 = arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2]
+            total = sum1 + sum2 + sum3
+            if total >= best:
+                best = total
+    return best
 
 
 arr = []
