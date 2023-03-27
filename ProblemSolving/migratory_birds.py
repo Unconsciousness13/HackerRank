fc69c4670e334035124384a3546c0e131c84f525
unconsciousness13 <pakounicredit@gmail.com>
2023-03-27 11:14:17 +0200

    migratoiry birds

---
+++ ProblemSolving/migratory_birds.py
@@ -0,0 +1,12 @@
+def migratoryBirds(arr):
+    # Write your code here
+    pass
+
+
+arr_count = int(input().strip())
+
+arr = list(map(int, input().rstrip().split()))
+
+result = migratoryBirds(arr)
+
+print(str(result) + '\n')
